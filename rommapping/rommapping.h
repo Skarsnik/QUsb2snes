4ca9fa957c0e95633e8f91f872a9f02517aa4cea
Benjamin Marwell <bmarwell@gmail.com>
2021-04-11 17:48:29 +0200

    [#77] add license headers and checks

--- rommapping/rommapping.h
+++ rommapping/rommapping.h
@@ -1,21 +1,22 @@
 /*
-Copyright 2016 Sylvain "Skarsnik" Colinet
-
- This file is part of the sneshackingtools project.
-
-    sneshackingtools is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Foobar is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with sneshackingtools.  If not, see <http://www.gnu.org/licenses/>
-    */
+ * Copyright (c) 2021 the QUsb2Snes authors.
+ *
+ * This file is part of QUsb2Snes.
+ * (see https://github.com/Skarsnik/QUsb2snes).
+ *
+ * QUsb2Snes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QUsb2Snes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QUsb2Snes.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef H_ROMMAPPING_H
 #define H_ROMMAPPING_H
